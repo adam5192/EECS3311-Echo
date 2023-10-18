de6ae05949b25f1f24e65148be7020c099b6c3bc
tran-tk <77289465+tran-tk@users.noreply.github.com>
2023-10-18 19:03:23 -0400

    Update Log.java
    Add more tests to main method and adjust Date values.
    Suppressed deprecation warnings.

--- App/Log.java
+++ App/Log.java
@@ -1,126 +1,55 @@
 package EECS3311_Project.App;
 import java.util.*;
 import javax.management.InvalidAttributeValueException;
-
-//TODO: Implement storing and accessing database
-public class Log {
-   private int userId;
-   private Date loggedDate;
-   private int logType; //0 for basic, 1 for data, 2 for meal, 3 for exercise
-
-   //Constructors
-   public Log(int userId) {
-      this(null, userId);
-   }
-
-   public Log(Date logDate, int userId) {
-      loggedDate = logDate;
-      this.userId = userId;
-      logType = 0;
-   }
-
-   //Setters
-   public void setDate(Date logDate) {
-      this.loggedDate = logDate;
-   }
-
-   public void setLogType(int type) {
-      this.logType = type;
-   }
-
-   //Getters
-   public Date getDate() {
-      return loggedDate;
-   }
-
-   public int getLogType() {return logType;}
-
-   //toString format: YY/MM/DD 
-   @Override
-   public String toString() {
-      //TODO: Deprecated date-time object, change implementation
-      return String.format("%d/%2d/%2d", loggedDate.getYear(), loggedDate.getMonth(), loggedDate.getDate()); 
-   }
-
-   //TODO: Implement database queries and calorie/exercise intake logging
-   public static void main(String[] args) {
-        // examples
-        Ingredient tomato = new Ingredient("Tomato", 20, 0, 1, 6);
-        Ingredient bread = new Ingredient("Bread", 255, 3, 8, 51);
-        Ingredient egg = new Ingredient("Egg", 145, 11, 14, 2);
-
-        // create Meal object
-        int dummyID = 0;
-        MealLog breakfast = new MealLog(dummyID);
-        breakfast.setType("Breakfast");
-        breakfast.setDate(new Date(2023, 10, 14));
-        breakfast.addIngredient(tomato);
-        breakfast.addIngredient(bread);
-        breakfast.addIngredient(egg);
-
-        System.out.println("Total calories: " + breakfast.calculateCalories());
-        System.out.println("Total protein: " + breakfast.calculateProtein());
-        System.out.println("Total fat: " + breakfast.calculateFat());
-        System.out.println("Total carbs: " + breakfast.calculateCarbs());
-
-        // create meal logger object and add breakfast to it
-        MealLogger myMealLogger = new MealLogger();
-        myMealLogger.logMeal(breakfast);
-
-        // display all meals logged in the MealLogger
-        System.out.println("All logged meals: " + myMealLogger.getMeals());
-   }
-}
+import javax.xml.crypto.Data;
 
 //For logging height and weight changes.
 class DataLog extends Log {
-   private double loggedHeight;
-   private double loggedWeight;
+   private double logHeight;
+   private double logWeight;
 
    //Constructors
    public DataLog(int userId) {
       super(userId);
-      loggedHeight = 0.0;
-      loggedWeight = 0.0;
+      logHeight = 0.0;
+      logWeight = 0.0;
       super.setLogType(1);
    }
 
    public DataLog(double height, double weight, Date logDate, int userId) {
       super(logDate, userId);
-      loggedHeight = height;
-      loggedWeight = weight;
+      logHeight = height;
+      logWeight = weight;
       super.setLogType(1);
    }
 
+   //Setters
+   public void setLogHeight(double height) {
+      try {
+         if (height < 0.0) throw new InvalidAttributeValueException("Height cannot be negative.");
+         this.logHeight = height;
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
+   public void setLogWeight(double weight) {
+      try {
+         if (weight < 0.0) throw new InvalidAttributeValueException("Weight cannot be negative.");
+         this.logWeight = weight;
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
    //Getters
-   public double getLoggedHeight() {return this.loggedHeight;}
-   public double getLoggedWeight() {return this.loggedWeight;}
-   
-   //Might not need setters
+   public double getLogHeight() {return this.logHeight;}
+   public double getLogWeight() {return this.logWeight;}
 
    //toString format: YY/MM/DD - Height, Weight
    public String toString() {
-      return super.toString() + String.format(" - Height: %.2f, Weight: %.2f", loggedHeight, loggedWeight);
+      return super.toString() + String.format(" - Height: %.2f, Weight: %.2f", logHeight, logWeight);
    }
-}
-
-//For testing output of mealLog (to be moved to Profile as a function of querying)
-class MealLogger {
-    private List<MealLog> meals; //this list holds each meal
-
-    public MealLogger() {
-        meals = new ArrayList<>();
-    }
-
-    //add meal to list
-    public void logMeal(MealLog meal) {
-        meals.add(meal);
-    }
-
-    //return logged meals
-    public List<MealLog> getMeals() {
-        return meals;
-    }
 }
 
 //For logging individual meals
@@ -134,6 +63,7 @@ class MealLog extends Log {
       super(userId);
       caloIn = 0;
       mealType = "Breakfast";
+      ingredients = new ArrayList<Ingredient>();
       super.setLogType(2);
    }
 
@@ -147,6 +77,7 @@ class MealLog extends Log {
 
          this.caloIn = caloIn;
          this.mealType = mealType;
+         ingredients = new ArrayList<Ingredient>();
          super.setLogType(2);
       } catch (Exception e) {
          e.printStackTrace();
@@ -162,20 +93,20 @@ class MealLog extends Log {
    public String getType() {return mealType;}
 
    //Setters
-   public void setCaloIn(int newIn) {
+   public void setType(String mealType) {
       try {
-         if (newIn < 0) throw new InvalidAttributeValueException("Calorie intake cannot be negative.");
-         caloIn = newIn;
+         if (!(mealType.equals("Breakfast") || mealType.equals("Lunch") || mealType.equals("Dinner") || mealType.equals("Snack"))) 
+            throw new InvalidAttributeValueException("Invalid mealType value.");
+         this.mealType = mealType;
       } catch (Exception e) {
          e.printStackTrace();
       }
    }
 
-   public void setType(String mealType) {
+   public void setCaloIn(int newIn) {
       try {
-         if (!(mealType.equals("Breakfast") || mealType.equals("Lunch") || mealType.equals("Dinner"))) 
-            throw new InvalidAttributeValueException("Invalid mealType value.");
-         this.mealType = mealType;
+         if (newIn < 0) throw new InvalidAttributeValueException("Calorie intake cannot be negative.");
+         caloIn = newIn;
       } catch (Exception e) {
          e.printStackTrace();
       }
@@ -196,6 +127,7 @@ class MealLog extends Log {
    //Add ingredients to the meal
    public void addIngredient(Ingredient in) {
       ingredients.add(in);
+      caloIn += in.getCalories(); //Update calorie intake of the log
    }
 
    //Calculate the calorie, protein, fat and carb content of the meal
@@ -215,69 +147,18 @@ class MealLog extends Log {
       return ingredients.stream().mapToInt(Ingredient::getCarbs).sum();
    }
 
-   //toString format: YY/MM/DD - Meal Type - Amount
+   //toString format: YY/MM/DD - Meal Type - Calorie Intake
+   @Override
    public String toString() {
       return super.toString() + String.format(" - %s - Calorie Intake: %d", mealType, caloIn);
    }
-}
 
-class ExerciseLog extends Log {
-   //TODO: Implement to match Calculator
-   private int caloBurnt;
-   private double time;
-
-   //Constructor
-   public ExerciseLog(int userId) {
-      super(userId);
-      caloBurnt = 0;
-      time = 0.0;
-      super.setLogType(3);
-   }
-
-   public ExerciseLog(int caloBurnt, double time,Date logDate, int userId) {
-      super(logDate, userId);
-      this.caloBurnt = caloBurnt;
-      this.time  = time;
-      try {
-         if (caloBurnt < 0) 
-            throw new InvalidAttributeValueException("Value of calorie burnt cannot be negative.");
-         else if (time < 0.0)
-            throw new InvalidAttributeValueException("Value of time cannot be negative.");
-
-         this.caloBurnt = caloBurnt;
-         this.time = time;
-         super.setLogType(3);
-      } catch (Exception e) {
-         e.printStackTrace();
+   public String getIngredientString() {
+      String out = "";
+      for (int i = 0 ; i < ingredients.size(); i++) {
+         out += ingredients.get(i).toString() + "\n";
       }
-   }
-
-   //Setters
-   public void setCaloBurnt(int caloBurnt) {
-      try {
-         if (caloBurnt < 0) throw new InvalidAttributeValueException("The value of calorie burnt cannot be negative.");
-         this.caloBurnt = caloBurnt;
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-   }
-
-   public void setTime(double time) {
-      try {
-         if (time < 0.0) throw new InvalidAttributeValueException("Time spent exercising cannot be negative.");
-         this.time = time;
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-   }
-
-   //Getters
-   public int getCaloBurnt() {return this.caloBurnt;}
-   public double getTime() {return this.time;}
-
-   //toString format: Date - Time exercised - Calorie burnt
-   public String toString() {
-      return super.toString() + String.format(" - %.2f min - %d cal burnt", time, caloBurnt);
+      return out;
    }
 }
 
@@ -358,4 +239,137 @@ class Ingredient {
         return "Ingredient{" + "name= " + name + '\'' + ", calories = " + calories + ", fat = " + fat + ", protein = " + protein + ", carbs = " + carbs + '}';
     }
 
+}
+
+class ExerciseLog extends Log {
+   private int caloBurnt;
+   private double time;
+
+   //Constructor
+   public ExerciseLog(int userId) {
+      super(userId);
+      caloBurnt = 0;
+      time = 0.0;
+      super.setLogType(3);
+   }
+
+   public ExerciseLog(int caloBurnt, double time, Date logDate, int userId) {
+      super(logDate, userId);
+      this.caloBurnt = caloBurnt;
+      this.time  = time;
+      setCaloBurnt(caloBurnt);
+      setTime(time);
+   }
+
+   //Setters
+   public void setCaloBurnt(int caloBurnt) {
+      try {
+         if (caloBurnt < 0) throw new InvalidAttributeValueException("The value of calorie burnt cannot be negative.");
+         this.caloBurnt = caloBurnt;
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
+   public void setTime(double time) {
+      try {
+         if (time < 0.0) throw new InvalidAttributeValueException("Time spent exercising cannot be negative.");
+         this.time = time;
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
+   //Getters
+   public int getCaloBurnt() {return this.caloBurnt;}
+   public double getTime() {return this.time;}
+
+   //toString format: Date - Time exercised - Calorie burnt
+   public String toString() {
+      return super.toString() + String.format(" - %.2f min - %d cal burnt", time, caloBurnt);
+   }
+}
+
+@SuppressWarnings("deprecation")
+//TODO: Implement storing and accessing database
+public class Log {
+   private int userId;
+   private Date loggedDate;
+   private int logType; //0 for basic, 1 for data, 2 for meal, 3 for exercise
+
+   //Constructors
+   public Log(int userId) {
+      this(new Date(), userId);
+      loggedDate.setYear(loggedDate.getYear()+1900);
+   }
+
+   public Log(Date logDate, int userId) {
+      loggedDate = logDate;
+      this.userId = userId;
+      logType = 0;
+   }
+
+   //Setters
+   public void setDate(Date logDate) {
+      this.loggedDate = logDate;
+   }
+
+   public void setLogType(int type) {
+      this.logType = type;
+   }
+
+   //Getters
+   public Date getDate() {
+      return loggedDate;
+   }
+
+   public int getLogType() {return logType;}
+
+   //toString format: YY/MM/DD 
+   @Override
+   public String toString() {
+      return String.format("%d/%2d/%2d", loggedDate.getYear(), loggedDate.getMonth()+1, loggedDate.getDate()); 
+   }
+
+   //TODO: Implement database queries and calorie/exercise intake logging
+   public static void main(String[] args) {
+      //DataLog
+      int dummyID = 0;
+      DataLog data = new DataLog(dummyID);
+      System.out.println(data.toString());
+      data.setLogHeight(170);
+      data.setLogWeight(80);
+      System.out.println(data.toString());
+
+      data = new DataLog(180, 70, new Date(2023, 9, 15), dummyID);
+      System.out.println(data.toString()+"\n");
+
+      //MealLog
+      // examples
+      Ingredient tomato = new Ingredient("Tomato", 20, 0, 1, 6);
+      Ingredient bread = new Ingredient("Bread", 255, 3, 8, 51);
+      Ingredient egg = new Ingredient("Egg", 145, 11, 14, 2);
+
+      // create Meal object
+      MealLog breakfast = new MealLog(dummyID);
+      breakfast.setType("Breakfast");
+      breakfast.setDate(new Date(2023, 9, 14));
+      breakfast.addIngredient(tomato);
+      breakfast.addIngredient(bread);
+      breakfast.addIngredient(egg);
+
+      // display all meals logged in the MealLogger
+      System.out.println(breakfast.toString());
+      System.out.println(breakfast.getIngredientString());
+
+      //ExerciseLog
+      ExerciseLog exercise = new ExerciseLog(dummyID);
+      System.out.println(exercise.toString());
+      exercise.setCaloBurnt(420);
+      exercise.setTime(30);
+      System.out.println(exercise.toString());
+
+      exercise = new ExerciseLog(600, 120, new Date(2023, 9, 19), dummyID);
+      System.out.println(exercise.toString()+"\n");
+   }
 }
