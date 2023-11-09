cc2fda8cabb1cfbcf7713f13d73af71cabcb6a2a
adam5192 <92131288+adam5192@users.noreply.github.com>
2023-11-09 11:30:17 -0500

    Add files via upload

--- App/exerciseLog.java
+++ App/exerciseLog.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class Exercise  {
+class Exercise {
     private String date; // date of exercise
     private String time;
     private String type; // walking, running, etc...
@@ -14,47 +14,6 @@ class Exercise  {
         this.time = time;
         this.type = type;
         this.duration = duration;
-        this.intensity = intensity;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public String getIntensity() {
-        return intensity;
-    }
-
-    // Setters
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public void setIntensity(String intensity) {
         this.intensity = intensity;
     }
 
@@ -97,24 +56,24 @@ class ExerciseLogger {
 
 // TODO: Implement database helper
 
-//public class exerciseLog {
-//    public static void main(String[] args) {
-//        // sample bmr, this will be retrieved from user profile
-//        double userBMR = 2000;
-//
-//        // create an example exercise
-//        Exercise morningJog = new Exercise("2023-10-19", "09:30", "jogging", 25, "medium");
-//
-//        // Display the estimated calories burnt during the walk.
-//        System.out.println("Calories burnt: " + morningJog.calculateCaloriesBurnt(userBMR));
-//
-//        // create and exercise logger that logs all exercises
-//        ExerciseLogger myExerciseLogger = new ExerciseLogger();
-//
-//        // add morningJog to logger
-//        myExerciseLogger.logExercise(morningJog);
-//
-//        // displays all logged exercises
-//        System.out.println("logged exercises: " + myExerciseLogger.getExercises());
-//    }
-//}
+public class exerciseLog {
+    public static void main(String[] args) {
+        // sample bmr, this will be retrieved from user profile
+        double userBMR = 2000;
+
+        // create an example exercise
+        Exercise morningJog = new Exercise("2023-10-19", "09:30", "jogging", 25, "medium");
+
+        // Display the estimated calories burnt during the walk.
+        System.out.println("Calories burnt: " + morningJog.calculateCaloriesBurnt(userBMR));
+
+        // create and exercise logger that logs all exercises
+        ExerciseLogger myExerciseLogger = new ExerciseLogger();
+
+        // add morningJog to logger
+        myExerciseLogger.logExercise(morningJog);
+
+        // displays all logged exercises
+        System.out.println("logged exercises: " + myExerciseLogger.getExercises());
+    }
+}
