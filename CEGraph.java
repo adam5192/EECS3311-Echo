1923799bf2f653023d30c2fff9fdcd670624c2f3
avdez03 <91922774+avdez03@users.noreply.github.com>
2023-11-18 21:17:57 -0500

    Add files via upload

---
+++ CEGraph.java
@@ -0,0 +1,189 @@
+/**
+ * Team Echo
+ * EECS 3311
+ * Use case 4 that visualizes the daily calorie intakes
+ * and daily exercise within an inputed time period
+ */
+package ECHO.UseCase4Test;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYSplineRenderer;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+
+@SuppressWarnings("serial")
+public class CEGraph extends JFrame implements ActionListener {
+	private String startDate;
+	private String endDate;
+	private JLabel inputDate;
+	private JTextField start;
+	private JLabel to;
+	private JTextField end;
+	private JLabel example;
+	private JButton graph;
+
+	public CEGraph(Profile user, MealLogger myMealLogger, ExerciseLogger myExerciseLogger) {
+		// Set window title
+		super("Daily Calory Intake & Daily Exercise");
+		// Set charts region
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(2, 0));
+
+		// Create top bar which takes time period from the user
+		inputDate = new JLabel("Input Date");
+		start = new JTextField(10);
+		to = new JLabel("To");
+		end = new JTextField(10);
+		example = new JLabel("dd/mm/yyyy");
+		graph = new JButton("Graph");
+		// programs button to create the chart
+		graph.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				startDate = start.getText();
+				endDate = end.getText();
+				try {
+					createTimeSeries(panel, startDate, endDate, Calculator.calculateBMR(user), myMealLogger,
+							myExerciseLogger);
+				} catch (ParseException e1) {
+					e1.printStackTrace();
+				}
+			}// end actionPerformeds
+		});// end actionPerformed
+
+		JPanel header = new JPanel();
+		header.add(inputDate);
+		header.add(start);
+		header.add(to);
+		header.add(end);
+		header.add(example);
+		header.add(graph);
+
+		getContentPane().add(header, BorderLayout.NORTH);
+		getContentPane().add(panel, BorderLayout.WEST);
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}// end UC5test
+
+	public void createTimeSeries(JPanel panel, String startDate, String endDate, double userBMR,
+			MealLogger myMealLogger, ExerciseLogger myExerciseLogger) throws ParseException {
+		panel.removeAll();// removes previous chart that was made
+		panel.revalidate();
+		panel.repaint();
+		TimeSeries caloryIntake = new TimeSeries("Daily Calory Intake");
+		TimeSeries amountOfExercise = new TimeSeries("Daily Amount of Exercise");
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		int day, month, year;
+		// turns date inputed from the user into a format
+		// that allows comparison of dates
+		Date start = simpleDateFormat.parse(startDate);
+		Date end = simpleDateFormat.parse(endDate);
+		// will receive the user BMR from profile
+		// will receive myMealLogger, log of the users meals, from mealLog
+		// will receive myExerciseLogger, log of the users
+		// exercise info, from exerciseLog
+		// catch when the user inputs an end date that is
+		// earlier than the start date
+
+		if (start.after(end)) {
+			JOptionPane.showMessageDialog(null, "Incorrect Date Info");
+		} else {
+			// for loop that adds info to the chart that fits
+			// into the inputed time period
+			for (int i = 0; i < myMealLogger.getMeals().size(); i++) {
+				Date mealDate = simpleDateFormat.parse(myMealLogger.getMeals().get(i).getDate());
+				if (mealDate.equals(start) || mealDate.equals(end) || mealDate.after(start) && mealDate.before(end)) {
+					day = Integer.valueOf(myMealLogger.getMeals().get(i).getDate().substring(0, 2));
+					month = Integer.valueOf(myMealLogger.getMeals().get(i).getDate().substring(3, 5));
+					year = Integer.valueOf(myMealLogger.getMeals().get(i).getDate().substring(6, 10));
+					caloryIntake.add(new Day(day, month, year), myMealLogger.getMeals().get(i).calculateCalories());
+				} // end if statement
+			} // end for loop
+
+			for (int i = 0; i < myExerciseLogger.getExercises().size(); i++) {
+				Date exerciseDate = simpleDateFormat.parse(myExerciseLogger.getExercises().get(i).getDate());
+				if (exerciseDate.equals(start) || exerciseDate.equals(end)
+						|| exerciseDate.after(start) && exerciseDate.before(end)) {
+					day = Integer.valueOf(myExerciseLogger.getExercises().get(i).getDate().substring(0, 2));
+					month = Integer.valueOf(myExerciseLogger.getExercises().get(i).getDate().substring(3, 5));
+					year = Integer.valueOf(myExerciseLogger.getExercises().get(i).getDate().substring(6, 10));
+					amountOfExercise.add(new Day(day, month, year),
+							myExerciseLogger.getExercises().get(i).calculateCaloriesBurnt(userBMR));
+				} // end if statement
+			} // end for loop
+				// creates the chart and adds the data
+				// adds the different axis'
+			TimeSeriesCollection dataset = new TimeSeriesCollection();
+			dataset.addSeries(caloryIntake);
+
+			TimeSeriesCollection dataset2 = new TimeSeriesCollection();
+			dataset2.addSeries(amountOfExercise);
+
+			XYPlot plot = new XYPlot();
+			XYSplineRenderer splinerenderer1 = new XYSplineRenderer();
+			XYSplineRenderer splinerenderer2 = new XYSplineRenderer();
+
+			plot.setDataset(0, dataset);
+			plot.setRenderer(0, splinerenderer1);
+			DateAxis domainAxis = new DateAxis("Days");
+			domainAxis.setAutoTickUnitSelection(false);
+			plot.setDomainAxis(domainAxis);
+			domainAxis.setDateFormatOverride(new SimpleDateFormat("dd/MM/yy"));
+			plot.setRangeAxis(new NumberAxis("Calories(cal)"));
+
+			plot.setDataset(1, dataset2);
+			plot.setRenderer(1, splinerenderer2);
+			plot.setRangeAxis(1, new NumberAxis("Exercise"));
+
+			plot.mapDatasetToRangeAxis(0, 0);
+			plot.mapDatasetToRangeAxis(1, 1);
+
+			JFreeChart chart = new JFreeChart("Daily Calory Intake & Daily Amount of Exercise",
+					new Font("Serif", java.awt.Font.BOLD, 18), plot, true);
+
+			ChartPanel chartPanel = new ChartPanel(chart);
+			chartPanel.setPreferredSize(new Dimension(900, 600));
+			chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+			chartPanel.setBackground(Color.white);
+			panel.add(chartPanel);
+			panel.revalidate();
+			panel.repaint();
+		} // end if statement
+	}// end createTimeSeries
+
+	public static void main(String[] args) {
+		Profile user = new Profile();
+		MealLogger myMealLogger = new MealLogger();
+		ExerciseLogger myExerciseLogger = new ExerciseLogger();
+		new CEGraph(user, myMealLogger, myExerciseLogger);
+	}// end main
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	}// end actionPerformed
+}// end class
---
+++ DNGraph.java
@@ -0,0 +1,181 @@
+/**
+ * Team Echo
+ * EECS 3311
+ * Use case 5 that visualizes the daily
+ * nutrient intakes
+ */
+package ECHO.UseCase5Test;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
+@SuppressWarnings("serial")
+public class DNGraph extends JFrame implements ActionListener {
+	private String startDate;
+	private String endDate;
+	private JLabel inputDate;
+	private JTextField start;
+	private JLabel to;
+	private JTextField end;
+	private JLabel example;
+	private JButton graphTen;
+	private JButton graphFive;
+
+	public DNGraph(MealLogger myMealLogger) {
+		// Set window title
+		super("Daily Nutrients Intake");
+
+		inputDate = new JLabel("Input Date");
+		start = new JTextField(10);
+		to = new JLabel("To");
+		end = new JTextField(10);
+		example = new JLabel("dd/mm/yyyy");
+		graphTen = new JButton("Graph Top 10");
+		graphFive = new JButton("Graph Top 5");
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(2, 0));
+		JPanel header = new JPanel();
+		graphTen.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				startDate = start.getText();
+				endDate = end.getText();
+				try {
+					createChartTopTen(panel, startDate, endDate, myMealLogger);
+				} catch (ParseException e1) {
+					e1.printStackTrace();
+				}
+			}// end actionPerformed
+		});// end actionPerformed
+		graphFive.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				startDate = start.getText();
+				endDate = end.getText();
+				try {
+					createChartTopFive(panel, startDate, endDate, myMealLogger);
+				} catch (ParseException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}// end actionPerformed
+
+		});// end actionPerformed
+		header.add(inputDate);
+		header.add(start);
+		header.add(to);
+		header.add(end);
+		header.add(example);
+		header.add(graphTen);
+		header.add(graphFive);
+
+		getContentPane().add(header, BorderLayout.NORTH);
+		getContentPane().add(panel, BorderLayout.WEST);
+		setExtendedState(JFrame.MAXIMIZED_BOTH);// make fullscreen
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}// end DNGraph
+
+	private void createChartTopTen(JPanel panel, String startDate, String endDate, MealLogger myMealLogger)
+			throws ParseException {
+		panel.removeAll();// removes any previous charts that where made
+		panel.revalidate();
+		panel.repaint();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Date start = simpleDateFormat.parse(startDate);
+		Date end = simpleDateFormat.parse(endDate);
+		// data set
+		DefaultPieDataset result = new DefaultPieDataset();
+
+		if (startDate.equals("") || endDate.equals("") || start.after(end)) {
+			JOptionPane.showMessageDialog(null, "Incorrect Date Info");
+		} else {
+			JFreeChart chart = ChartFactory.createPieChart("Daily Nutrient Intake", result, true, true, false);
+
+			ChartPanel chartPanel = new ChartPanel(chart);
+			chartPanel.setPreferredSize(new Dimension(900, 600));
+			chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+			chartPanel.setBackground(Color.white);
+			panel.add(chartPanel);
+			panel.revalidate();
+			panel.repaint();
+		} // end if statement
+	}// end createChartTopTen
+
+	private void createChartTopFive(JPanel panel, String startDate, String endDate, MealLogger myMealLogger)
+			throws ParseException {
+		panel.removeAll();// removes any previous charts that where made
+		panel.revalidate();
+		panel.repaint();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Date start = simpleDateFormat.parse(startDate);
+		Date end = simpleDateFormat.parse(endDate);
+		// data set
+		DefaultPieDataset result = new DefaultPieDataset();
+		int totalCalories = 0;
+		int totalFat = 0;
+		int totalProtein = 0;
+		int totalCarbs = 0;
+		int other = 0;
+
+		for (Meal meal : myMealLogger.getMeals()) {
+			Date mealDate = simpleDateFormat.parse(meal.getDate());
+			if (mealDate.equals(start) || mealDate.equals(end) || mealDate.after(start) && mealDate.before(end)) {
+				totalCalories += meal.calculateCalories();
+				totalFat += meal.calculateFat();
+				totalProtein += meal.calculateProtein();
+				totalCarbs += meal.calculateCarbs();
+				other += meal.calculateOthers();
+			} // end if statement
+		} // end for loop
+			// adds nutrients to the data set
+		result.setValue("Carbs", totalCarbs);
+		result.setValue("Protein", totalProtein);
+		result.setValue("Fat", totalFat);
+		result.setValue("Calories", totalCalories);
+		result.setValue("Other", other);
+		// catch when the user inputs a end date that is earlier than the start
+		if (startDate.equals("") || endDate.equals("") || start.after(end)) {
+			JOptionPane.showMessageDialog(null, "Incorrect Date Info");
+		} else {
+			JFreeChart chart = ChartFactory.createPieChart("Daily Nutrient Intake", result, true, true, false);
+
+			ChartPanel chartPanel = new ChartPanel(chart);
+			chartPanel.setPreferredSize(new Dimension(900, 600));
+			chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+			chartPanel.setBackground(Color.white);
+			panel.add(chartPanel);
+			panel.revalidate();
+			panel.repaint();
+		} // end if statement
+	}// end createChartTopFive
+
+	public static void main(String[] args) {
+		MealLogger myMealLogger = new MealLogger();
+		new DNGraph(myMealLogger);
+	}// end main
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	}// end actionPerformed
+}// end class
