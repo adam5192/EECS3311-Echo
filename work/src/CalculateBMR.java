418a1451781c3247ca7b46ddd2f8842748dffc51
Omer2099 <92131901+Omer2099@users.noreply.github.com>
2023-11-20 23:08:36 -0500

    Update CalculateBMR.java

--- work/src/CalculateBMR.java
+++ work/src/CalculateBMR.java
@@ -2,10 +2,34 @@
 public class CalculateBMR {
 
 	
-	public static double calculateBMR(boolean gender,  double weight, double height, int bmrSetting, double bodyFat) 
-	{
-		return 113;
-		}
+	public static double calculateBMR(Date date, double weight, double height, Boolean gender, int bmrSetting, double FatLvl) 
+    {
+        double bmr = 0;
+        int age = ( LocalDate.now().getYear() )- ( date.getYear() );
+        
+        // Miffin St Jeor BMR calculation
+        if (bmrSetting == 0) { 
+          if (gender) {
+            bmr = (10 * weight + (6.25 * height ) - (5 * age) + 5);
+          } else {
+            bmr = (10 * weight + (6.25 * height ) - (5 * age) - 161);
+          }
+        } 
+        // Revised Harris-Benedict BMR calculation 
+        else if (bmrSetting == 1) {
+          if (gender) {
+            bmr = (13.397 * weight + (4.799 * height ) - (5.677 * age) + 88.362);
+          } else {
+            bmr = (9.247 * weight + (3.098 * height ) - (4.330 * age) + 447.593);
+          }
+        } 
+        //Katch McArdle BMR calculation
+        else if (bmrSetting == 2) {
+          bmr = (370 + 21.6 * (1 - ( FatLvl / 100)) * FatLvl );
+        }
+        
+    	return (Math.round(bmr));
+    }
 
 	
 }
