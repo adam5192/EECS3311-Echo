60e2cb2d9580ec29ab23c158d492e84f7bb75ec1
adam5192 <92131288+adam5192@users.noreply.github.com>
2023-11-27 10:51:37 -0500

    Update ProfileGui.java

--- EECS3311-Echo-main/EECS3311-Echo-main/work/src/ProfileGui.java
+++ EECS3311-Echo-main/EECS3311-Echo-main/work/src/ProfileGui.java
@@ -8,26 +8,21 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.util.Date;
 
-import javax.swing.ButtonGroup;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JLabel;
-import javax.swing.JRadioButton;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 public class ProfileGui implements ActionListener, FocusListener {
-	
+
 	MyFrame frame = new MyFrame();
 	JButton Create = new JButton("Create profile");
 	JButton back = new JButton("Back");
 	JTextField Height = new JTextField("Height");
 	JTextField Weight = new JTextField("Weight");
 	JTextField BodyFat = new JTextField("BodyFat%");
-	JTextField date = new JTextField("yy/mm/dd");
+	JTextField date = new JTextField("yyyy/mm/dd");
 	JRadioButton Male = new JRadioButton("Male");
 	JRadioButton Female = new JRadioButton("Female");
-	JRadioButton Metric = new JRadioButton("Metric"); 
-	JRadioButton Imperial = new JRadioButton("Imperial"); 
+	JRadioButton Metric = new JRadioButton("Metric");
+	JRadioButton Imperial = new JRadioButton("Imperial");
 
 	JRadioButton setting1 = new JRadioButton("Miffin St Jeor");
 	JRadioButton setting2 = new JRadioButton("Revised Harris-Benedict");
@@ -37,14 +32,14 @@ public class ProfileGui implements ActionListener, FocusListener {
 	JLabel GenderButtonsLabel = new JLabel("Gender");
 	JLabel UnitButtonsLabel = new JLabel("Unit");
 	JLabel BMRSettingLabel = new JLabel("BMR setting");
-	
+
 	ButtonGroup group = new ButtonGroup();
 	ButtonGroup groupSettings = new ButtonGroup();
 	ButtonGroup UnitGroups = new ButtonGroup();
 
 
 	Front front;
-	
+
 	public static Profile currProfile;
 	double height;
 	double weight;
@@ -55,19 +50,19 @@ public class ProfileGui implements ActionListener, FocusListener {
 	Date birth;
 	String Birth;
 
-	
 
-	 public ProfileGui(Front front) {
-		 
+
+	public ProfileGui(Front front) {
+
 		this.front = front;
-		 
+
 		frame.setTitle("Profile");
-		 
+
 		// frame.setLayout(new FlowLayout(FlowLayout.CENTER, 100, 20));
 		back.setBounds(350, 20, 70, 30);
-		 
-		frame.setVisible(false);
-		 
+
+		frame.setVisible(true);
+
 		title.setBounds(230, 0, 100, 70);
 		title.setForeground(Color.BLACK);
 
@@ -75,11 +70,11 @@ public class ProfileGui implements ActionListener, FocusListener {
 		Weight.setBounds(200, 85, 100, 30);
 		BodyFat.setBounds(200, 120, 100, 30);
 		date.setBounds(200, 155, 100, 30);
-		
+
 		GenderButtonsLabel.setBounds(65, 175, 100, 70);
 		Male.setBounds(20, 220, 65, 30);
 		Female.setBounds(85, 220, 75, 30);
-		
+
 		BMRSettingLabel.setBounds(200, 250, 100, 70);
 		setting1.setBounds(100, 300, 100, 30);
 		setting2.setBounds(250, 300, 200, 30);
@@ -88,19 +83,19 @@ public class ProfileGui implements ActionListener, FocusListener {
 		UnitButtonsLabel.setBounds(350, 175, 100, 70);
 		Metric.setBounds(280, 225, 100, 30);
 		Imperial.setBounds(380, 225, 100, 30);
-		
+
 		Create.setBounds(180, 400, 120, 40);
-		
+
 		group.add(Male);
 		group.add(Female);
 
 		groupSettings.add(setting1);
 		groupSettings.add(setting2);
 		groupSettings.add(setting3);
-		
+
 		UnitGroups.add(Metric);
 		UnitGroups.add(Imperial);
-		
+
 		frame.add(back);
 		frame.add(title);
 		frame.add(Height);
@@ -118,23 +113,21 @@ public class ProfileGui implements ActionListener, FocusListener {
 		frame.add(UnitButtonsLabel);
 		frame.add(BMRSettingLabel);
 		frame.add(Create);
-		
+
 		Weight.addFocusListener(this);
 		Height.addFocusListener(this);
 		BodyFat.addFocusListener(this);
 		date.addFocusListener(this);
-		
+
 		back.addActionListener(this);
 		Create.addActionListener(this);
-		
+
 		frame.setLayout(null);
 		frame.setLocationRelativeTo(null);
-		frame.setVisible(true); //makes frame visible
-			
-	 }
+	}
 
 	public ProfileGui(Front front, int user) {
-		
+
 		back.setBounds(350, 20, 70, 30);
 
 		title.setBounds(230, 20, 100, 70);
@@ -159,7 +152,7 @@ public class ProfileGui implements ActionListener, FocusListener {
 		dateTitle.setBounds(120, 270, 100, 30);
 		BMRTitle.setBounds(120, 310, 100, 30);
 
-		
+
 
 		frame.add(title);
 		frame.add(Userid);
@@ -174,24 +167,22 @@ public class ProfileGui implements ActionListener, FocusListener {
 		back.addActionListener(this);
 		frame.setLayout(null);
 		frame.setLocationRelativeTo(null);
-		frame.setVisible(true); //makes frame visible
-
 	}
 
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getSource()==back) 
+		if(e.getSource()==back)
 		{
 			frame.setVisible(false);
-            front.main.setVisible(true);
+			front.main.setVisible(true);
 		}
-		else if(e.getSource()==Male) 
+		else if(e.getSource()==Male)
 		{
 			gender=true;
 		}
-		else if(e.getSource()==Female) 
+		else if(e.getSource()==Female)
 		{
 			gender=false;
 		}
@@ -215,46 +206,60 @@ public class ProfileGui implements ActionListener, FocusListener {
 		{
 			unitSetting=false;
 		}
-		else if(e.getSource()==Create) 
+		else if(e.getSource()==Create)
 		{
-			height =  Double.parseDouble(Height.getText());
-			weight =  Double.parseDouble(Weight.getText());
-			fatlevel =  Double.parseDouble(BodyFat.getText());
+			try {
+				height =  Double.parseDouble(Height.getText());
+				weight =  Double.parseDouble(Weight.getText());
+				fatlevel =  Double.parseDouble(BodyFat.getText());
+			} catch (NumberFormatException ex) {
+				JOptionPane.showMessageDialog(frame, "Please enter valid numbers");
+			}
 			Birth = date.getText();
-			
-			birth = new Date(Integer.parseInt(Birth.substring(0,4)),Integer.parseInt(Birth.substring(5,7))-1,Integer.parseInt(Birth.substring(8,10)));
-			
-			currProfile = new Profile(gender, birth, height, weight, bmrSetting);
+			try {
+				birth = new Date(Integer.parseInt(Birth.substring(0,4)),Integer.parseInt(Birth.substring(5,7))-1,Integer.parseInt(Birth.substring(8,10)));
+			} catch (StringIndexOutOfBoundsException ex) {
+				JOptionPane.showMessageDialog(frame, "Incorrect date format");
+			} catch (NumberFormatException exc) {
+				JOptionPane.showMessageDialog(frame, "Please enter valid date");
+			}
+
+			try {
+				currProfile = new Profile(gender, birth, height, weight, bmrSetting);
+				JOptionPane.showMessageDialog(frame, "Profile Created");
+			} catch (NullPointerException exception) {
+				JOptionPane.showMessageDialog(frame, "Please fill in all fields correctly");
+			}
 		}
-		
+
 	}
-	
-	  @Override
-	  public void focusGained(FocusEvent e) {
-	    // TODO Auto-generated method stub
 
-	    JTextField textField = (JTextField) e.getSource();
-	    if ( textField.getText().equals("Weight") || textField.getText().equals("Height") || textField.getText().equals("BodyFat%")
-	    		|| textField.getText().equals("yy/mm/dd")) {
-	      textField.setText("");
-	    } 
+	@Override
+	public void focusGained(FocusEvent e) {
+		// TODO Auto-generated method stub
 
-	  }
+		JTextField textField = (JTextField) e.getSource();
+		if ( textField.getText().equals("Weight") || textField.getText().equals("Height") || textField.getText().equals("BodyFat%")
+				|| textField.getText().equals("yyyy/mm/dd")) {
+			textField.setText("");
+		}
 
-	  @Override
-	  public void focusLost(FocusEvent e) {
-	    // TODO Auto-generated method stub
-	    JTextField textField = (JTextField) e.getSource();
-	    if (textField.getText().isEmpty()) {
-	       if (textField == Weight) {
-	        textField.setText("Weight");
-	      } else if (textField == Height) {
-	        textField.setText("Height");
-	      } else if (textField == BodyFat) {
-	        textField.setText("BodyFat%");
-	      } else if (textField == date) {
-		        textField.setText("yy/mm/dd");
-		      }
-	    }
-	  }
+	}
+
+	@Override
+	public void focusLost(FocusEvent e) {
+		// TODO Auto-generated method stub
+		JTextField textField = (JTextField) e.getSource();
+		if (textField.getText().isEmpty()) {
+			if (textField == Weight) {
+				textField.setText("Weight");
+			} else if (textField == Height) {
+				textField.setText("Height");
+			} else if (textField == BodyFat) {
+				textField.setText("BodyFat%");
+			} else if (textField == date) {
+				textField.setText("yyyy/mm/dd");
+			}
+		}
+	}
 }
