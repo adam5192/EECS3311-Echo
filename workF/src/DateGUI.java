3dbaab30f4473fe7f6a075b878a54f629a10b231
adam5192 <92131288+adam5192@users.noreply.github.com>
2023-12-04 18:46:27 -0500

    Added DateGUI.java
    handles duplicate code that was previously found in both mealGUI and exerciseGUI

---
+++ workF/src/DateGUI.java
@@ -0,0 +1,114 @@
+import javax.swing.*;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+public class DateGUI {
+    // This method checks if the inputted date by the user is a real date
+    public static boolean isValidDate(int year, int month, int day) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month - 1); // Calendar month is 0-based
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+
+        int maxDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+        return day >= 1 && day <= maxDay;
+    }
+
+    public static void setupComboDateBoxes(JComboBox yearComboBox, JComboBox dayComboBox, JComboBox monthComboBox){
+        String[] months = {
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+        };
+        // Year JComboBox
+        for (int year = 2020; year <= 2025; year++) {
+            yearComboBox.addItem(year);
+        }
+
+        // Month JComboBox setup
+        for (int i = 0; i < months.length; i++) {
+            monthComboBox.addItem(months[i]);
+        }
+
+        // Day combo box setup
+        for (int day = 1; day <= 31; day++) {
+            dayComboBox.addItem(day);
+        }
+    }
+
+    public static void logThis(JComboBox yearComboBox, JComboBox dayComboBox, JComboBox monthComboBox, String objType, Front front, Meal meal, Exercise exercise, List<String> optionsList) throws ParseException {
+        boolean validDateSelected = false;
+        while (!validDateSelected) {
+            // Add elements to panel for date selection
+            JPanel panel = new JPanel();
+            panel.add(new JLabel("Year:"));
+            panel.add(yearComboBox);
+            panel.add(Box.createHorizontalStrut(15)); // Spacer
+            panel.add(new JLabel("Month:"));
+            panel.add(monthComboBox);
+            panel.add(Box.createHorizontalStrut(15)); // Spacer
+            panel.add(new JLabel("Day:"));
+            panel.add(dayComboBox);
+            // Show the dialog for date selection
+
+            int result = JOptionPane.showOptionDialog(
+                    null,
+                    panel,
+                    "Enter the Date",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    new Object[]{
+                            "OK"
+                    }, // Array of options. Only "OK" button here
+                    null // Default button, null for no default
+            );
+
+            // Process the result
+            if (result == JOptionPane.OK_OPTION) {
+                int year = (int) yearComboBox.getSelectedItem();
+                int day = (int) dayComboBox.getSelectedItem(); // validate this input
+                int month = monthComboBox.getSelectedIndex()+1;
+
+                // Check if the date is valid
+                if (isValidDate(year, month, day)) {
+                    String formattedDate = year + "/" + String.format("%02d", month) + "/" + String.format("%02d", day);
+                    System.out.println("Selected Date: " + formattedDate);
+                    validDateSelected = true;
+
+                    if(objType.equals("Meal")){
+                        meal.setDate(formattedDate);
+                        System.out.println(front.mealGUIInstance.mealLogger.getMeals());
+                        // Remove all logged meal types except snacks
+                        front.mealGUIInstance.mealLogger.getMeals().stream()
+                                .filter(m -> m.getDate().equals(formattedDate))
+                                .map(Meal::getMealType)
+                                .distinct()
+                                .forEach(mealType -> {
+                                    if (!"Snacks".equals(mealType)) {
+                                        optionsList.remove(mealType);
+                                    }
+                                });
+                    } else if(objType.equals("Exercise")){
+                        exercise.setDate(formattedDate);
+                    }
+                } else {
+                    System.out.println("Invalid Date");
+                    JOptionPane.showMessageDialog(panel, "Please enter valid date");
+                }
+            }
+        }
+    }
+}
